8665d3b2b81d6bf378b0d9351af482fd2bcf0250
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-16 04:46:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,13 +24,13 @@ class Application
           end
         end
     elsif req.path.match(/add/)
-        search_term = req.params["items"]
-          if @@items.include?(items)
-              @@cart << items
-              resp.write "added #{items}"
-          else
-              resp.write "We don't have that item"
-            end
+        search_term = req.params["item"]
+        if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+        else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
